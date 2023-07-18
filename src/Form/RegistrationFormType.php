685f0787c438d1dd49888db99b40126b2bcdd764
Tuncay Alp <alptuncay5@gmail.com>
2023-07-19 00:40:44 +0200

    add checkbox

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,15 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Range;
 
 class RegistrationFormType extends AbstractType
@@ -47,6 +47,15 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('agreeTerms', CheckboxType::class, [
+              'label' => "J'accepte les conditions générales d'utilisation",
+              'mapped' => false,
+              'constraints' => [
+                  new IsTrue([
+                      'message' => 'Vous devez accepter les conditions générales d\'utilisation.',
+                  ]),
+              ],
+          ])
         ;
     }
 
